91c601204e06849b9852a7cd05c9babfd6cfc0aa
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-01-24 22:50:34 -0500

    Reformatted lua

--- init.lua
+++ init.lua
@@ -3,63 +3,63 @@ require('andrew')
 -- install lazy.nvim package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	-- fuzzy finder
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim'}
-	},
-	-- color schemes
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
+    -- fuzzy finder
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim'}
+    },
+    -- color schemes
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
     },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	-- treesitter
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{"nvim-treesitter/nvim-treesitter-textobjects", dependencies = {"nvim-treesitter/nvim-treesitter"}},
+    -- treesitter
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {"nvim-treesitter/nvim-treesitter-textobjects", dependencies = {"nvim-treesitter/nvim-treesitter"}},
 
-	-- lsp zero
-	--- Uncomment the two plugins below if you want to manage the language servers from neovim
-	--- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
+    -- lsp zero
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
-	-- null-ls (now none-ls) for static typing and linting
-	{'nvimtools/none-ls.nvim'},
-	-- mason-null-ls manages non-lsp installations for Mason
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		}
-	},
-	-- debugging
-	{'mfussenegger/nvim-dap'},
-	{'mfussenegger/nvim-dap-python', dependencies = {'mfussenegger/nvim-dap'}},
-	{'rcarriga/nvim-dap-ui', dependencies = {'mfussenegger/nvim-dap'}},
-	-- code commenting
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    -- null-ls (now none-ls) for static typing and linting
+    {'nvimtools/none-ls.nvim'},
+    -- mason-null-ls manages non-lsp installations for Mason
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        }
+    },
+    -- debugging
+    {'mfussenegger/nvim-dap'},
+    {'mfussenegger/nvim-dap-python', dependencies = {'mfussenegger/nvim-dap'}},
+    {'rcarriga/nvim-dap-ui', dependencies = {'mfussenegger/nvim-dap'}},
+    -- code commenting
     {'numToStr/Comment.nvim'},
     -- tmux integration
     {
@@ -95,4 +95,4 @@ local plugins = {
     },
 }
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup(plugins, {})
