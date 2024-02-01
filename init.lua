2bcfa533642179ef8cf8ed6d321ff4940b17145a
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-02-01 11:49:12 -0500

    Add dadbod and slime to config

--- init.lua
+++ init.lua
@@ -92,6 +92,27 @@ local plugins = {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
+    -- send commands
+    {'jpalardy/vim-slime'},
+    -- sql
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            { 'tpope/vim-dadbod', lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    }
+
 }
 
 require("lazy").setup(plugins, {})
