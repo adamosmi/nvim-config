aa7be69985a6889f90fe28b8eee947481e5237f3
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-02-05 16:04:19 -0500

    Map vim-slime to space, enter

--- after/plugin/vim-slime.lua
+++ after/plugin/vim-slime.lua
@@ -6,3 +6,7 @@ vim.g.slime_default_config = {
 vim.g.slime_bracketed_paste = 1
 vim.g.slime_dont_ask_default = 1
 vim.g.slime_preserve_curpos = 0
+
+-- map space, enter to vim-slime, preserves the original keymap
+vim.cmd('map <leader><CR> <C-c><C-c>')
+
