f63ce7c6b154cd8d3efb3537f26d8c770432b0de
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-02-05 13:17:46 -0500

    Add pyright config

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -18,8 +18,10 @@ require('mason-lspconfig').setup({
     },
 })
 
+-- lsp setup
+-- lua
 -- get rid of the undefined global vim error when writing config
-require('lspconfig')['lua_ls'].setup {
+require('lspconfig').lua_ls.setup({
     settings = {
         Lua = {
             diagnostics = {
@@ -27,5 +29,28 @@ require('lspconfig')['lua_ls'].setup {
             }
         }
     }
-}
+})
 
+-- python
+require('lspconfig').pyright.setup({
+    -- Callback function for when the language server attaches to a buffer
+    on_attach = on_attach,
+    settings = {
+        pyright = {
+            -- Enables automatic import completions, suggesting import statements for unrecognized symbols
+            autoImportCompletion = true,
+        },
+        python = {
+            analysis = {
+                -- Automatically adds common search paths (like "src") for module resolution
+                autoSearchPaths = true,
+                -- Limits analysis and error reporting to only files that are currently open
+                diagnosticMode = "openFilesOnly",
+                -- Allows analysis of library code for type information in the absence of type stubs
+                useLibraryCodeForTypes = true,
+                -- Sets the type checking mode to 'off', disabling static type checking
+                typeCheckingMode = "off"
+            }
+        }
+    }
+})
