ed5d7b7fdcb707ac79ff5b117f0e4c6d0ce3b11f
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-04-13 12:19:29 -0400

    Disable ruff due to N/A diagnostics

--- after/plugin/mason-null-ls.lua
+++ after/plugin/mason-null-ls.lua
@@ -1,5 +1,6 @@
 -- define autogroup for auto formatting
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+-- local null_ls = require("null-ls")
 
 -- define non-lsp mason sources
 require("mason").setup()
@@ -14,27 +15,27 @@ require("mason-null-ls").setup({
 	automatic_installation = false,
 	handlers = {},
 })
-require("null-ls").setup({
-	sources = {
-		-- Anything not supported by mason.
-		require("null-ls").builtins.diagnostics.ruff,
-	},
-	-- auto-formatting
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({
-				group = augroup,
-				buffer = bufnr,
-			})
-
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end
-
-})
+-- null_ls.setup({
+-- 	sources = {
+-- 		-- Anything not supported by mason.
+-- 		null_ls.builtins.diagnostics.ruff,
+-- 	},
+-- 	-- auto-formatting
+-- 	on_attach = function(client, bufnr)
+-- 		if client.supports_method("textDocument/formatting") then
+-- 			vim.api.nvim_clear_autocmds({
+-- 				group = augroup,
+-- 				buffer = bufnr,
+-- 			})
+--
+-- 			vim.api.nvim_create_autocmd("BufWritePre", {
+-- 				group = augroup,
+-- 				buffer = bufnr,
+-- 				callback = function()
+-- 					vim.lsp.buf.format({ bufnr = bufnr })
+-- 				end,
+-- 			})
+-- 		end
+-- 	end
+--
+-- })
